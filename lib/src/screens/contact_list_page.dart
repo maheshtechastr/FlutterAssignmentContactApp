3eefad81743fede608a9a63eb411894fc40adb48
MaheshGupta <mahesh.gupta@nagarro.com>
2020-05-20 15:15:49 +0530

    Updated List item

--- lib/src/screens/contact_list_page.dart
+++ lib/src/screens/contact_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:contactapp/src/blocs/contact_bloc.dart';
 import 'package:contactapp/src/models/contact.dart';
 import 'package:contactapp/src/screens/add_edit_contact.dart';
+import 'package:contactapp/src/screens/list_item.dart';
 import 'package:flutter/material.dart';
 
 class ContactListPage extends StatefulWidget {
@@ -77,63 +78,3 @@ class ContactListState extends State<ContactListPage> {
   }
 }
 
-class ListItem extends StatelessWidget {
-  final Contact contact;
-
-  ListItem({Key key, this.contact}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () => {
-              print("List Item clicked==>" + contact.name),
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AddEditContact(
-                            contact: contact,
-                            pageTitle: "Update Contact",
-                          )))
-            },
-        child: Row(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    width: 60.0,
-                    height: 60.0,
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey,
-                        image: new DecorationImage(
-                            fit: BoxFit.fill,
-                            image: new NetworkImage(
-                                "https://i.imgur.com/BoN9kdC.png")))),
-              ],
-            ),
-            Flexible(
-                child: Container(
-                    padding: EdgeInsets.only(left: 10.0, right: 10),
-                    child: Column(
-                        //mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            contact.name,
-                            textScaleFactor: 1.5,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            contact.mobNumber,
-                            style: TextStyle(fontWeight: FontWeight.w300),
-                            textScaleFactor: 1.2,
-                            textAlign: TextAlign.left,
-                          )
-                        ])))
-          ],
-        ));
-  }
-}
--- lib/src/screens/favorites_page.dart
+++ lib/src/screens/favorites_page.dart
@@ -1,6 +1,7 @@
 import 'package:contactapp/src/blocs/favorite_contact_bloc.dart';
 import 'package:contactapp/src/models/contact.dart';
 import 'package:contactapp/src/screens/add_edit_contact.dart';
+import 'package:contactapp/src/screens/list_item.dart';
 import 'package:flutter/material.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -69,63 +70,4 @@ class FContactListState extends State<FavoritesPage> {
   }
 }
 
-class ListItem extends StatelessWidget {
-  final Contact contact;
 
-  ListItem({Key key, this.contact}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () => {
-              print("List Item clicked==>" + contact.name),
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AddEditContact(
-                            contact: contact,
-                            pageTitle: "Update Contact",
-                          )))
-            },
-        child: Row(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    width: 60.0,
-                    height: 60.0,
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey,
-                        image: new DecorationImage(
-                            fit: BoxFit.fill,
-                            image: new NetworkImage(
-                                "https://i.imgur.com/BoN9kdC.png")))),
-              ],
-            ),
-            Flexible(
-                child: Container(
-                    padding: EdgeInsets.only(left: 10.0, right: 10),
-                    child: Column(
-                        //mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            contact.name,
-                            textScaleFactor: 1.5,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            contact.mobNumber,
-                            style: TextStyle(fontWeight: FontWeight.w300),
-                            textScaleFactor: 1.2,
-                            textAlign: TextAlign.left,
-                          )
-                        ])))
-          ],
-        ));
-  }
-}
